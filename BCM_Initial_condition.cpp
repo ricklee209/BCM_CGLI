e2e9003ab6d226663e38b69b4115e7596c66bf3d
CGLI <chung-gang.li@riken.jo>
2016-01-18 15:41:03 +0900

    fixed bugs

--- BCM_Initial_condition.cpp
+++ BCM_Initial_condition.cpp
@@ -366,11 +366,6 @@ void BCM_Initial_condition
 					U1_[icube][i][j][k][3] = U1[icube][i][j][k][3]; 
 					U1_[icube][i][j][k][4] = U1[icube][i][j][k][4]; 
 
-					U1q[icube][i][j][k][0] = U1[icube][i][j][k][0]; 
-					U1q[icube][i][j][k][1] = U1[icube][i][j][k][1]; 
-					U1q[icube][i][j][k][2] = U1[icube][i][j][k][2]; 
-					U1q[icube][i][j][k][3] = U1[icube][i][j][k][3]; 
-					U1q[icube][i][j][k][4] = U1[icube][i][j][k][4]; 
 					
 				}
 			}
