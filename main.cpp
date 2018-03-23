ea96046c43850dfcf81c4079ae48a6ef36638f24
Li_KobeU <chung-gang.li@riken.jp>
2018-03-23 16:04:12 +0900

    nothing

--- main.cpp
+++ main.cpp
@@ -1653,7 +1653,7 @@ int main(int argc, char **argv)
             
             #if CR == 1 
                 
-                BCM_Output_coarse(myid, Ncube, step, switch_output, rank_map, U1_,U1q,cube_size, Xcnt, Ycnt, Zcnt);
+                BCM_Output_coarse(myid, Ncube, step, switch_output, rank_map, U1_,U1q,cube_size, Xcnt, Ycnt, Zcnt, filter);
                 
             #endif
             
